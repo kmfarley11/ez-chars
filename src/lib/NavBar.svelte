38110700a9347ef3cc710e7c8cdaba183489e53d
kmfarley11 <kmfarley11@gmail.com>
2026-02-27 13:30:52 -0500

    TODO burndowns - refactor our NavBar etc.

---
+++ src/lib/NavBar.svelte
@@ -0,0 +1,40 @@
+<script>
+	import AboutButton from '$lib/AboutButton.svelte';
+	import DropDownMenu from '$lib/DropDownMenu.svelte';
+	import GitButton from '$lib/GitButton.svelte';
+	import Heading from '$lib/Heading.svelte';
+	import HomeButton from '$lib/HomeButton.svelte';
+	import MenuItemButton from '$lib/MenuItemButton.svelte';
+</script>
+
+<nav>
+	<div class="flex w-full flex-wrap items-center justify-between border bg-slate-700 px-1 py-1 text-white">
+		<div class="items-center">
+			<DropDownMenu variant="dark">
+				<MenuItemButton onclick={() => alert('todo')}>2014 5e Character</MenuItemButton>
+			</DropDownMenu>
+		</div>
+		<div class="items-center">
+			<Heading variant="md"><a href="/">ez-chars</a></Heading>
+		</div>
+		<div class="hidden w-full md:block md:w-auto" id="navbar-default">
+			<ul class="flex-row items-center font-medium flex">
+				<li>
+					<div class="block rounded-sm">
+						<GitButton variant="dark" />
+					</div>
+				</li>
+				<li>
+					<div class="block rounded-sm">
+						<HomeButton variant="dark" />
+					</div>
+				</li>
+				<li>
+					<div class="block rounded-sm">
+						<AboutButton variant="dark" />
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,49 +1,11 @@
 <script>
-	import AboutButton from '$lib/AboutButton.svelte';
-	import DropDownMenu from '$lib/DropDownMenu.svelte';
-	import GitButton from '$lib/GitButton.svelte';
-	import Heading from '$lib/Heading.svelte';
-	import HomeButton from '$lib/HomeButton.svelte';
-	import MenuItemButton from '$lib/MenuItemButton.svelte';
+	import NavBar from '$lib/NavBar.svelte';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
 
 <div>
-	<!-- TODO: abstract into navbar? -->
-	<!-- TODO: why is routing broken in prod? (404 err) turn off SPA? -->
-	<!-- TODO: why does home btn route to my resume site? -->
-	<nav>
-		<div class="flex w-full flex-wrap items-center justify-between border bg-slate-700 px-1 py-1 text-white">
-			<div class="items-center">
-				<DropDownMenu variant="dark">
-					<MenuItemButton onclick={() => alert('todo')}>2014 5e Character</MenuItemButton>
-				</DropDownMenu>
-			</div>
-			<div class="items-center">
-				<Heading variant="md"><a href="/">ez-chars</a></Heading>
-			</div>
-			<div class="hidden w-full md:block md:w-auto" id="navbar-default">
-				<ul class="flex-row items-center font-medium flex">
-					<li>
-						<div class="block rounded-sm">
-							<GitButton variant="dark"/>
-						</div>
-					</li>
-					<li>
-						<div class="block rounded-sm">
-							<HomeButton variant="dark" />
-						</div>
-					</li>
-					<li>
-						<div class="block rounded-sm">
-							<AboutButton variant="dark" />
-						</div>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
+	<NavBar />
 	{@render children()}
 </div>
